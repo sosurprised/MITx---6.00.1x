e9567524e7990661f98ca9f144f2e7cf84757888
Nadia Andrushchenko <41171731+sosurprised@users.noreply.github.com>
2019-03-26 14:41:01 -0300

    Add files via upload

---
+++ PS1-1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 14:35:32 2019
+
+@author: gecap
+"""
+#counting vowels in a string
+
+s='anhtarehs'
+numVowels=0    
+for char in s: 
+    if char == 'o' or char == 'u' or char == 'e' or char == 'a' or char == 'i':
+        numVowels+=1
+print ('Number of vowels is: ' + str(numVowels))
